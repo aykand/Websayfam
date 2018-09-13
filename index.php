64936cea4671b88c4da2044cc1d6a7f88466f43a
aykand <43254003+aykand@users.noreply.github.com>
2018-09-14 01:02:20 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,309 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<style type="text/css"><?php include 'main.php'; ?></style>
+
+<head>
+    <link rel="shortcut icon" href="https://adresgezginitasarim.com/aykan/instagram.png" />
+    <meta charset="utf-8">
+    <title>İnstagram Reklamları</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="800 milyon kullanıcısı olan Instagram sosyal ağına hızlı ve kolay reklam vermek için...">
+    <meta name="keywords" content="İnstagram, Instagram, Reklam,Instagram Reklamları">
+     </head>
+
+<body>
+    <link rel="alternate">
+    <div class="wrapper">
+        <div class="container">
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header page-scroll">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand nav-white page-scroll" href="https://adresgezginitasarim.com/aykan/">İnstagram Reklamları</a> </div>
+                    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav nav-white">
+                            <li><a class="page-scroll" href="#main" ;>Ana Sayfa</a></li>
+                            <li><a class="page-scroll" href="#features" ;>Hizmetlerimiz</a></li>
+                            <li><a class="page-scroll" href="#galeri" ;>Galeri</a></li>
+                            <li><a class="page-scroll" href="#who" ;>Biz Kimiz?</a></li>
+                            <li><a class="page-scroll" href="#tür" ;>Reklam Türleri</a></li>
+                            <li><a class="page-scroll" href="#contact" ;>İletişim</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="main image-bg" id="main">
+            <div class="hero-section">
+                <div class="container nopadding">
+                    <div class="col-md-8">
+                        <div class="hero-content">
+                            <h1 class="wow fadeInUp" data-wow-delay="0s">İnstagram Reklamları</h1>
+                            <p class="wow fadeInUp" data-wow-delay="0.2s"> 800 milyon kullanıcısı olan bu platforma
+                                <br class="hidden-xs"> hızlı ve kolay reklam vermek için </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4" id="pricing">
+                        <div class="span3">
+                            <form action="mail/mail.php?islem=iletisim"  method="post">
+                                <input type="text" style="" name="name" class="span3" placeholder="Adınız Soyadınız *" required="">
+                                <input type="email" name="email" class="span3" placeholder="E-posta Adresiniz *" required="">
+                                <input type="phone" name="phone" class="span3" placeholder="Telefon Numaranız" required="">
+                                <input type="text" name="website" class="span3" placeholder="Websiteniz *" required="">
+                           <textarea name="mesaj" required="" placeholder="Mesajınızı Giriniz"></textarea>
+                                <input type="submit" value="Formu Gönder" class="btn btn-primary pull-right">
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div id="features" class="app-features text-center">
+                <div class="container">
+                    <h1 class="wow fadeInDown" data-wow-delay="0.1s">İnstagram Reklamları</h1>
+                    <p class="wow fadeInDown" data-wow-delay="0.2s"> 300 milyondan fazla bir toplulukla İnstagram dünyanın en büyük mobil reklam platformlarından biri haline geldi.
+                        <br class="hidden-xs">Bu gelişme ile işletmeler haberlerini yaratıcı, yüksek kaliteli bir ortamda, etkileşim oranı yüksek bir hedef kitleyle paylaşabiliyor. </p>
+                    <div class="col-md-4 features-left">
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.2s">
+                            <div class="icon"> <i class="fab fa-google"></i> </div>
+                            <div class="feature-single">
+                                <p> Google Ads ve Facebook Reklam çalışmalarına, bu yeni ve güçlü platformu da ekleyerek müşterilerimize daha geniş ve doğru kitleler için reklam verme olanağı sunuyoruz. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.3s">
+                            <div class="icon"><i class="fab fa-facebook"></i> </div>
+                            <div class="feature-single">
+                                <p> Facebook Reklamları ile oldukça benzer bir çalışma imkanı tanıyan İnstagram, pazarlama, marka bilinirliğini artırma, yüksek geri dönüş oranı konularında reklam verenlere faydalı olacağına güveniyor. </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 wow fadeInDown" data-wow-delay="0.5s"> <img class="img-responsive" src="assets/images/pexels-photo.jpg" alt="Yumruklarını tokuşturan insanlar" width="350" height="650" /> </div>
+                    <div class="col-md-4 features-left">
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.6s">
+                            <div class="icon"> <i class="fab fa-instagram"></i> </div>
+                            <div class="feature-single">
+                                <p> İnstagram kullanıcılarının ilgi alanlarına göre reklamverenler, mesajlarını daha etkili olarak aktarabilir ve kitlelerine daha kolay ulaşabilirler. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.7s">
+                            <div class="icon"> <i class="fab fa-slideshare"></i> </div>
+                            <div class="feature-single">
+                                <p> Bunun yanında, reklamverenler medyalarını artık yeni formatlardan birinde paylaşma imkanına sahipler. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="galeri" class="gallery">
+                <h2 class="wow fadeInUp " data-wow-delay="0.1s">Galeri</h2>
+                <div class="gallery-container">
+                    <div class="gallery-item" data-index="1"> <img src="assets/images/6.jpg" alt="İnstagram mobil uygulamsı"> </div>
+                    <div class="gallery-item" data-index="2"><img src="assets/images/1.jpg" alt="Laptop üzerindeki telefon"> </div>
+                    <div class="gallery-item" data-index="3"><img src="assets/images/2.jpg" alt="İnstagram profil sayfası"> </div>
+                    <div class="gallery-item" data-index="4"><img src="assets/images/3.jpg" alt="Açık laptop"> </div>
+                    <div class="gallery-item" data-index="5"><img src="assets/images/4.jpg" alt="El sıkışan iki insan"> </div>
+                    <div class="gallery-item" data-index="6"><img src="assets/images/5.jpg" alt="İnstagram web sayfası"> </div>
+                </div>
+            </div>
+            <div id="who" class="pricing">
+                <div class="container">
+                    <div class="col-md-6">
+                        <div class="pricing-content">
+                            <h3 class="wow fadeInUp " data-wow-delay="0.1s">Biz kimiz?</h3>
+                            <br>
+                            <p class="wow fadeInUp" data-wow-delay="0.3s">2007 yılında İzmir Yüksek Teknoloji Enstitüsü bünyesindeki İzmir Teknoloji Geliştirme Bölgesi’nde üniversitenin Elektrik-Elektronik Mühendisliği bölümü bünyesinde çalışan akademisyenler Dr. Bora Mocan, Dr. Şükrü Ozan, Dr. İlker Tanyer ve Nejla Mocan tarafından kurulmuştur.AdresGezgini Anonim Şirketi, günümüzde 400.000TL ödenmiş sermayesi, İzmir ve İstanbul’daki 50’den fazla uzman personeliyle Türkiye’nin en büyük bilişim firmalarından bir tanesidir. Bilişim kategorisinde son 4 yıldır İzmir’de vergi rekortmenliği ödülü almış bir firma olan AdresGezgini A.Ş. bu başarısını sağladığı yüksek müşteri memnuniyetine, AR-GE kültürüne ve mühendislik altyapısına borçludur.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6"> <img class="img-responsive wow fadeIn" src="assets/images/group.jpg" alt="Laptop kullanan insanlar"  /> </div>
+                </div>
+            </div>
+            <div id="tür" class="features">
+                <div class="container">
+                    <div class="features-inner">
+                        <div class="col-md-6"> <img class="img-responsive wow fadeInDown" data-wow-delay="0.2s" src="assets/images/facebook.jpg" alt="Sosyal ağ" /> </div>
+                        <div class="col-md-5 col-md-offset-1 nopadding">
+                            <div class="features-list">
+                                <h3 class="wow fadeInDown">Reklam Türleri</h3>
+                                <p class="wow fadeInDown" data-wow-delay="0.2s" style="line-height: 1"> Her ölçekten işletmenin kullanımına açık olan reklamlar. </p>
+                                <br>
+                                <br>
+                                <ul class="wow fadeInDown" data-wow-delay="0.3s">
+                                    <li>Landscape fotoğraf ve videolar</li>
+                                    <li>30 saniyeye kadar uzayabilen videolar</li>
+                                    <li>Kısa zamanda geniş kitleye ulaşmak için kullanılabilecek Marquee</li>
+                                    <li>Facebook ile yapılan çapraz kampanyalar</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing">
+                <div class="container">
+                    <div class="col-md-6">
+                        <div class="pricing-content">
+                            <h3 class="wow fadeInUp " data-wow-delay="0.1s">Nasıl Reklam Verilir?</h3>
+                            <br>
+                            <p class="wow fadeInUp" data-wow-delay="0.3s">Doğrudan İnstagram üzerinden alışveriş yapmayı da sağlayacak olan bu reklamlar, Facebook için kullanılan Power Editor reklam aracı ile yayına alınabilecek.
+                                <br>Ölçebileceğimiz metrikler ise, gösterim, erişim, sıklık ve demografi.</p>
+                            <br>
+                            <h3 class="wow fadeInUp" data-wow-delay="0.1s">Reklamlar nerede yayınlanacak?</h3>
+                            <br>
+                            <p class="wow fadeInUp" data-wow-delay="0.3s">Mobil cihazlarda kullanıcının haber kaynağında yer alacak.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6"> <img class="img-responsive wow fadeIn" src="https://adresgezginitasarim.com/aykan/assets/images/g%C3%B6rsel.jpg" alt="İnstagram profil" /> </div>
+                </div>
+            </div>
+
+            <div id="contact" class="info">
+                <div class="container">
+                    <div class="col-md-4">
+                        <h1 style="font-style: bold; font-size: 20px">İnstagram Reklamları</h1>
+                        <p>
+                            <ul>
+                                <li><i class="fas fa-map-marked-alt"></i> İzmir - Folkart Towers</li>
+                                <br>
+                                <li><i class="fas fa-map-marker-alt"></i> İstanbul - Levent Ofis</li>
+                                <br>
+                                <li><i class="fas fa-map-signs"></i> Almanya - Dusseldorf</li>
+                                <br>
+                                <li><i class="fas fa-headset"></i> 0 850 360 0 964</li>
+                                <br>
+                                <li><i class="fas fa-envelope"></i> destek<!-- >@. -->@<!-- >@. -->adresgezgini<!-- >@. -->.com</li>
+                                <br>
+                            </ul>
+                            <br class="hidden-xs"> </p>
+                    </div>
+                    <div class="col-md-2">
+                        <h1 style="font-size: 18px">Hızlı Link</h1>
+                        <ul>
+                            
+                            <li></li>
+                            <br>
+                            <li></li>
+                            <br>
+                            <li><i class="fas fa-angle-double-right"></i><a class="page-scroll" href="#main">Anasayfa</a></li>
+                            <br>
+                            <li><i class="fas fa-angle-double-right"></i><a class="page-scroll" href="#features" >Hizmetlerimiz</a></li>
+                            <br>
+                            <li><i class="fas fa-angle-double-right"></i><a class="page-scroll" href="#galeri">Galeri</a></li>
+                            <br>
+                            <li><i class="fas fa-angle-double-right"></i><a class="page-scroll" href="#who">Biz Kimiz?</a></li>
+                            <br>
+                            <li><i class="fas fa-angle-double-right"></i><a class="page-scroll" href="#tür">Reklam Türleri</a></li>
+                            <br>
+                        </ul>
+                    </div>
+                    <div class="col-md-4" id="googleMap">
+                        
+                        <br>
+                    </div>
+                    <div id="apps" class="col-md-2" style="margin-top: 45px">
+                        <a href="https://play.google.com/store/apps/details?id=com.adresgezgini.blogs" target="_blank"><img src="assets/images/google-play-120x0.png" alt="AdresGezgini Google Play" style="display: block;" width="120" height="39"></a>
+                        <br>
+                        <a href="https://itunes.apple.com/tr/app/adresgezgini/id1281942078?mt=8" target="_blank"><img src="https://adresgezginitasarim.com/aykan/assets/images/appstore-120x0.png" alt="AdresGezgini AppStore" style="display: block;" width="120" height="39"></a>
+                    </div>
+                </div>
+            </div>
+
+            <footer id="footer">
+                <div class="container">
+                    <div class="row justify-content-center align-items-center text-center">
+                        <div class="social_icon light">
+                            <ul>
+                                <li style=" margin-top: 3.5px;"> <a href="https://www.facebook.com/AdresGezgini" target="_blank"><i class="fab fa-facebook-f" style="color: white;"></i></a> <a href="https://www.instagram.com/adresgezgini/" target="_blank"><i class="fab fa-instagram" style="color: white;"></i></a> <a href="https://twitter.com/AdresGezgini" target="_blank"><i class="fab fa-twitter" style="color: white;"></i></a> </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="copyright light text-center">© Copyright 2018 <a href="https://adresgezgini.com/" target="blank">AdresGezgini A.Ş</a>. All Rights Reserved </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+
+
+    <link defer rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+        <script type="text/javascript" defer src="assets/js/jquery-2.1.1.js"></script>
+        <script type="text/javascript" defer src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" defer  src="assets/js/plugins.js"></script>
+        <script type="text/javascript" defer src="assets/js/menu.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+
+        <script type="text/javascript" defer src="assets/js/custom.js"></script>
+        
+        <script defer type="text/javascript">
+
+            setTimeout(function(){
+                $("#googleMap") .html (' <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3124.487152167472!2d27.175191315171602!3d38.453313280603005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bbd8f7a928794f%3A0xdcce6e21aff9511f!2sAdresGezgini+A.%C5%9E.+AdWords+Premier+Partner+Reklam+Ajans%C4%B1!5e0!3m2!1str!2str!4v1534404959106" width="300" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>');
+            } ,5000);
+
+
+            const galleryItem = document.getElementsByClassName("gallery-item");
+            const lightBoxContainer = document.createElement("div");
+            const lightBoxContent = document.createElement("div");
+            const lightBoxImg = document.createElement("img");
+            const lightBoxPrev = document.createElement("div");
+            const lightBoxNext = document.createElement("div");
+            lightBoxContainer.classList.add("lightbox");
+            lightBoxContent.classList.add("lightbox-content");
+            lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+            lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
+            lightBoxContainer.appendChild(lightBoxContent);
+            lightBoxContent.appendChild(lightBoxImg);
+            lightBoxContent.appendChild(lightBoxPrev);
+            lightBoxContent.appendChild(lightBoxNext);
+            document.body.appendChild(lightBoxContainer);
+            let index = 1;
+
+            function showLightBox(n) {
+                if (n > galleryItem.length) {
+                    index = 1;
+                } else if (n < 1) {
+                    index = galleryItem.length;
+                }
+                let imageLocation = galleryItem[index - 1].children[0].getAttribute("src");
+                lightBoxImg.setAttribute("src", imageLocation);
+            }
+
+            function currentImage() {
+                lightBoxContainer.style.display = "block";
+                let imageIndex = parseInt(this.getAttribute("data-index"));
+                showLightBox(index = imageIndex);
+            }
+            for (let i = 0; i < galleryItem.length; i++) {
+                galleryItem[i].addEventListener("click", currentImage);
+            }
+
+            function slideImage(n) {
+                showLightBox(index += n);
+            }
+
+            function prevImage() {
+                slideImage(-1);
+            }
+
+            function nextImage() {
+                slideImage(1);
+            }
+            lightBoxPrev.addEventListener("click", prevImage);
+            lightBoxNext.addEventListener("click", nextImage);
+
+            function closeLightBox() {
+                if (this === event.target) {
+                    lightBoxContainer.style.display = "none";
+                }
+            }
+            lightBoxContainer.addEventListener("click", closeLightBox);
+        </script>
+        
+</body>
+
+</html>
